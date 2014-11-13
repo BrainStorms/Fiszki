c46566b4aa65edd9c7453f0108124a3d1bd92ae5
Platonn <platonn@platonn.com>
2014-11-13 09:36:46 +0100

    Devise installed. User authentication working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'flashcards#index'
+
   resources :flashcards
 
   # The priority is based upon order of creation: first created -> highest priority.
